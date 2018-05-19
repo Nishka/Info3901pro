488e5cd4eefd15b345ec3e125098b784e3f271cd
Nishka <taniskan@yahoo.com>
2018-05-19 18:35:12 -0500

    update field size

--- addclass.sql
+++ addclass.sql
@@ -1,6 +1,6 @@
 create table addclass(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-coursenumber varchar(100) NOT NULL,
-coursename varchar(100) NOT NULL,
+coursenumber varchar(20) NOT NULL, 
+coursename varchar(50) NOT NULL,
 UNIQUE(coursename, coursenumber)
 );
